fa50de5422a09816e3afca73e5bb0052b0ceb602
Jacqueline.Ong <jong@macalester.edu>
2023-08-31 02:10:21 -0500

    Fix about section

--- src/Home/About.js
+++ src/Home/About.js
@@ -1,13 +1,28 @@
 import React from "react";
-import AboutContent from "./AboutContent";
+import mario_adrian from "../assets/mario_adrian.jpg";
+import "./about.css";
 
 export function About() {
 	return (
-		<section className="specials-div role=about-container">
-			<div id="about" className="specials-div-header">
-				<h1>About</h1>
+		<section className="about">
+			<div id="about" className="container grid">
+				<div className="about-info">
+					<h1>About</h1>
+					<h2>Little Lemon Founders</h2>
+					<p>
+						We are Mario and Adrian and we started Little Lemon in 2023 from our
+						love of cooking.
+					</p>
+					<p>
+						If you can't find answers to your questions about our menu or dining
+						options on our website, feel free to contact us and we're happy to
+						help you plan your visit.
+					</p>
+				</div>
+				<figure className="about-image">
+					<img src={mario_adrian} alt="Mario and Adrian"></img>
+				</figure>
 			</div>
-			<AboutContent />
 		</section>
 	);
 }
